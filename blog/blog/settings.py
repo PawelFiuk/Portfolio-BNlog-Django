6cf1f2da495babd60b581cbfd04d84b5d4bb2ce1
Pawel <deflorennno@gmail.com>
2022-07-20 00:11:41 +0200

    Zmiana ustawien

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from decouple import config
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -9,13 +10,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6eoaofoqjq@%xncy!@)+!xmr=_f9$i^ft=a^u(3*xgyia^b1$v'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = []
-
 
 
 # Application definition
